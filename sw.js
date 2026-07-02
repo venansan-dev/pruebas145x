fd997cb8eae7993914b0b4b0044adb2742aa9d45
venansan-dev <venansan@gmail.com>
2026-07-02 15:24:54 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'guia-compostelana-v2066';
+const CACHE_NAME = 'guia-compostelana-v2067';
 const TILE_CACHE = 'guia-tiles-v5';
 const IMG_CACHE  = 'guia-imgs-v10';
 const LIB_CACHE  = 'guia-libs-v1';
